8c14883a61ae31cf6e77fe2d54c3689d0452babb
valeriavil <vvilchis296@gmail.com>
2026-04-16 08:49:02 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+// Mensaje de bienvenida
+console.log("Bienvenido a la página de Emprendimiento");
+
+// Scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
 
